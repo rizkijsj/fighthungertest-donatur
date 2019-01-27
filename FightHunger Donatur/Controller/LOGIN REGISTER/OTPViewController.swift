4d641b7c268cd444d010dbb4da886bf8eb500f3d
Julianti Cahyadi <cahyadijulianti@gmail.com>
2019-01-27 21:26:25 +0700

    (continue) otp screen validation

--- FightHunger Donatur/Controller/LOGIN REGISTER/OTPViewController.swift
+++ FightHunger Donatur/Controller/LOGIN REGISTER/OTPViewController.swift
@@ -24,6 +24,7 @@ class OTPViewController: UIViewController , UITextFieldDelegate{
          otpTxt3.backgroundColor = UIColor.clear
          otpTxt4.backgroundColor = UIColor.clear
         
+        otpTxt1.becomeFirstResponder()
         
         //add border
         addBottomBorder(textField: otpTxt1)
@@ -49,7 +50,67 @@ class OTPViewController: UIViewController , UITextFieldDelegate{
     
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
         
+        if((textField.text?.count)! < 1) && string.count > 0
+        {
+            if textField == otpTxt1
+            {
+                otpTxt2.becomeFirstResponder()
+            }
+            
+            if textField == otpTxt2
+            {
+                otpTxt3.becomeFirstResponder()
+            }
+            
+            if textField == otpTxt3
+            {
+                otpTxt4.becomeFirstResponder()
+            }
+            
+            if textField == otpTxt4
+            {
+                otpTxt4.resignFirstResponder()
+            }
+            
+            //showing text
+            textField.text = string
+            return false
+        }
         
+       else if((textField.text?.count)! >= 1) && string.count == 0
+        {
+            
+            if textField == otpTxt2
+            {
+                otpTxt1.becomeFirstResponder()
+            }
+            
+            if textField == otpTxt3
+            {
+                otpTxt2.becomeFirstResponder()
+            }
+            
+            if textField == otpTxt4
+            {
+                otpTxt3.becomeFirstResponder()
+            }
+            
+            if textField == otpTxt1
+            {
+                otpTxt1.resignFirstResponder()
+            }
+            
+            textField.text = ""
+            return false
+        }
+        
+        else if((textField.text?.count)! >= 1)
+        {
+            textField.text = string
+            return false
+        }
+        
+        return true
     }
 
 }
