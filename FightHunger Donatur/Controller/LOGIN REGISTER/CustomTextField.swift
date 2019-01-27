61f8c53f4f85d1773d7792df3ba3080817970338
Julianti Cahyadi <cahyadijulianti@gmail.com>
2019-01-27 10:15:47 +0700

    add customTextField

---
+++ FightHunger Donatur/Controller/LOGIN REGISTER/CustomTextField.swift
@@ -0,0 +1,21 @@
+//
+//  CustomTextField.swift
+//  FightHunger Donatur
+//
+//  Created by Julianti Cahyadi on 27/01/19.
+//  Copyright © 2019 Fantastic7. All rights reserved.
+//
+
+import UIKit
+
+class CustomTextField: UITextField {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
