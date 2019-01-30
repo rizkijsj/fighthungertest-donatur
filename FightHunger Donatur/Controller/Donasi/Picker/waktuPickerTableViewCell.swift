95983621278fb3d3b55ac5d5ce41bfb2d651a935
Julianti Cahyadi <cahyadijulianti@gmail.com>
2019-01-30 22:38:54 +0700

    donating storyboard

--- FightHunger Donatur/Controller/Donasi/Picker/waktuPickerTableViewCell.swift
+++ FightHunger Donatur/Controller/Donasi/Picker/waktuPickerTableViewCell.swift
@@ -60,8 +60,8 @@ class waktuPickerTableViewCell: UITableViewCell , UIPickerViewDelegate , UIPicke
     }
     
     
-   var hours = Array(0...23)
-   var minutes = Array(00...59)
+   var hours = Array(1...23)
+   var minutes = Array(01...59)
   
     
     @IBOutlet weak var textWaktu: CustomTextField!
