b08e022623822df8b90ae60eb14693e6ad7c427a
Atn010 <>
2019-02-19 17:33:28 +0700

    fix pop up dah oke

--- FightHunger Donatur/Controller/Kegiatan/PembatalanController.swift
+++ FightHunger Donatur/Controller/Kegiatan/PembatalanController.swift
@@ -12,33 +12,50 @@ class PembatalanController: UITableViewController {
 	@IBOutlet weak var textViewLabel: UITextView!
 	@IBOutlet weak var konfirmasiBtn: UIButton!
 	
+	var selectedIndex:IndexPath = IndexPath(row: 0, section: 0)
+	
+	
 	//var selectedIndexpath:IndexPath?
 	override func viewDidLoad() {
         super.viewDidLoad()
 
        konfirmasiBtn.layer.cornerRadius = 6.0
 	   textViewLabel.isHidden = true
+		textViewLabel.layer.borderColor = UIColor.black.cgColor
+		textViewLabel.layer.borderWidth = 1.0
     }
 
 	@IBAction func cancelBtn(_ sender: Any) {
 		self.dismiss(animated: true, completion: nil)
 	}
 	// MARK: - Table view data source
+	
+	func resetChecked(){
+		for inRow in 1...5 {
+			tableView.cellForRow(at: IndexPath(row: inRow, section: 0))?.accessoryType = .none
+		}
+	}
 
 	
 	override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
 		
 		if indexPath.section == 0
 		{
+			resetChecked()
 			if indexPath.row == 1 || indexPath.row == 2 || indexPath.row == 3 || indexPath.row == 4
 			{
 				tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+				
+				selectedIndex = indexPath
+				textViewLabel.isHidden = true
 			}
 			
 			else
 			{
 				tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+				selectedIndex = indexPath
 				textViewLabel.isHidden = false
+				
 			}
 		}
 		
